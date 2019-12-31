8d8c5fd81f2e9482cba1ede17258f1e1cda3206d
egarreau <ergarreau@gmail.com>
2019-12-30 18:22:08 -0600

    fix indentation

--- models/mailer.rb
+++ models/mailer.rb
@@ -3,26 +3,26 @@ class Mailer
   def self.send(filename, subject, send_type)
     if send_type == 'send to everyone'
       recipient = Reader.emails
-		else
-			recipient = nil
+    else
+      recipient = nil
     end
 
-		m = Pony.mail({
+    m = Pony.mail({
       :to => ENV['gmail_user'],
       :bcc => recipient,
       :from => ENV['gmail_user'],
-			:subject => subject,
-			:html_body => File.read(filename),
-			:via => :smtp,
-			:via_options => {
-				:address              => 'smtp.gmail.com',
-				:port                 => '587',
+      :subject => subject,
+      :html_body => File.read(filename),
+      :via => :smtp,
+      :via_options => {
+        :address              => 'smtp.gmail.com',
+        :port                 => '587',
         :user_name            => ENV['gmail_user'],
-				:password             => ENV['gmail_pass'],
-				:authentication       => :plain, 
-    		:domain               => "localhost.localdomain" 
-			} 
+        :password             => ENV['gmail_pass'],
+        :authentication       => :plain, 
+        :domain               => "localhost.localdomain" 
+      } 
     })
     puts m
-	end
+  end
 end
