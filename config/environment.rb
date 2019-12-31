f05c8c26ecfd6d8a364bebd8b89bb1910f05ad79
egarreau <ergarreau@gmail.com>
2019-12-30 19:25:40 -0600

    fix heroku issue

--- config/environment.rb
+++ config/environment.rb
@@ -1,8 +1,10 @@
 ENV['SINATRA_ENV'] ||= 'development'
 ENV['SINATRA_ACTIVESUPPORT_WARNING'] = 'false'
 
-require 'dotenv'
-Dotenv.load
+if ENV['SINATRA_ENV'] == 'development'
+	require 'dotenv'
+	Dotenv.load
+end
 
 require 'bundler/setup'
 Bundler.require(:default, ENV['SINATRA_ENV'])
