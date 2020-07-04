c4c5cefbf068eff0bc8f3b6893729f3b637da140
egarreau <ergarreau@gmail.com>
2020-07-04 13:29:26 -0500

    add popsicles letter

--- models/letter.rb
+++ models/letter.rb
@@ -2,7 +2,7 @@ class Letter
 
   def initialize(title, path)
     @subject = "May I Recommend ☞ #{title}"
-    @body = File.read(path)
+    @html = File.read(path)
     # @yaml = YAML.load_file(path)
   end
 
@@ -11,25 +11,26 @@ class Letter
       :from    => ["'Evangeline Garreau' #{ENV['gmail_user']}"],
       :to      => recipient,
       :subject => @subject,
-      :html    => @body
+      :html    => @html
     }
   end
 
-  def build_with_template(recipient)
-    {
-      :from    => ["'Evangeline Garreau' #{ENV['gmail_user']}"],
-      :to      => recipient,
-      :subject => @subject,
-      :template => 'newsletter',
-      ":h:X-Mailgun-Variables" => @body
-    }
-  end
+  # currently unused due to troubleshooting issues
+  # def build_with_template(recipient)
+  #   {
+  #     :from    => ["'Evangeline Garreau' #{ENV['gmail_user']}"],
+  #     :to      => recipient,
+  #     :subject => @subject,
+  #     :template => 'newsletter',
+  #     ":h:X-Mailgun-Variables" => @yaml
+  #   }
+  # end
 
   # currently unused due to the fact that it causes all replies to thread in one email
-  def batch
-    message = Mailgun::BatchMessage.new(@bottle, ENV['mg_domain'])
-    message.from(ENV['gmail_user'], {'first' => 'Evangeline', 'last' => 'Garreau'})
-    message.subject(@subject)
-    message.body_html(@html)
-  end
+  # def batch
+  #   message = Mailgun::BatchMessage.new(@bottle, ENV['mg_domain'])
+  #   message.from(ENV['gmail_user'], {'first' => 'Evangeline', 'last' => 'Garreau'})
+  #   message.subject(@subject)
+  #   message.body_html(@html)
+  # end
 end
